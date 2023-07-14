a05b877ba7042ae7059d8e21e8a510ac60f287cf
Haikal Fikri <haikaljr1245@gmail.com>
2023-07-14 07:55:37 +0700

    added post, put, patch, and delete to apiview

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 # Create your views here.
-class HelloAPiView(APIView):
+class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
     
     def get(self, request, format=None):
         """Returns a list of APIView featues"""
@@ -22,3 +26,32 @@ class HelloAPiView(APIView):
                 'an_apiview': an_apiview,
             }
         )
+        
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+    
+    def put(self, request, pk=None):
+        """Handle updating object"""
+        return Response({'method': 'PUT'})
+    
+    
+    def patch(self, request, pk=None):
+        """Handle patch object"""
+        return Response({'method': 'PATCH'})
+    
+    
+    def delete(self, request, pk=None):
+        """Handle delete object"""
+        return Response({'method':'DELETE'})
