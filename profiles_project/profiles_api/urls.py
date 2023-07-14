462bcb4bd81f72ea72f7df676a6c0f7be4c0f78e
Haikal Fikri <haikaljr1245@gmail.com>
2023-07-14 09:41:32 +0700

    added login api

--- profiles_project/profiles_api/urls.py
+++ profiles_project/profiles_api/urls.py
@@ -9,5 +9,6 @@ router.register('profile-viewset', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view(), name='hello-view'),
-    path('', include(router.urls))
+    path('login/', views.UserLoginApiView.as_view()),
+    path('', include(router.urls)),
 ]
